bab89a04521372c0e21c9a3c4c0311438cc820b4
Nish <dulan418@gmail.com>
2021-05-15 10:29:39 +0530

    2nd com

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -3,7 +3,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; <a href="" target="_blank">Dulan N</a> 2020</span>
+            <span>Copyright &copy; <a href="" target="_blank">Dulan N</a> 2021</span>
           </div>
         </div>
       </footer>
